45857345cb9f10eae7d78f7dbaf701f9d76e7388
Golonka-Ma <115418615+Golonka-Ma@users.noreply.github.com>
2025-10-10 17:48:40 +0200

    SEO optimize

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/oferta',
+        destination: '/',
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
